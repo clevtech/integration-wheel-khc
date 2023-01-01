c0fbfdd0738180a0bd781b51577874fd99fb0daa
Yeren Kalibek <yerenn22@gmail.com>
2023-01-01 22:46:11 +0600

    refactor: rename file extension

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,7 @@ import { Button, Container, Text } from '@chakra-ui/react';
 export default function Header() {
     return (
         <Container maxWidth='100%' background='green.600'>
-            <Container maxWidth='container.xl'>
-                <Text fontSize='4xl' color='white'>
-                    Hello World
-                </Text>
-            </Container>
+            <Container maxWidth='container.xl'></Container>
         </Container>
     );
 }
