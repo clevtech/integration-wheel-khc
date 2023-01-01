8030604c673e5241f1c9f8ab6b9ccdb347743f5e
Yeren Kalibek <yerenn22@gmail.com>
2023-01-02 04:21:06 +0600

    feat: add custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
+import Footer from './layouts/Footer';
 import Header from './layouts/Header';
+
+const theme = extendTheme({
+    colors: {
+        yellowKhc: {
+            100: '#D3B961',
+            200: '#C9A11C',
+        },
+        greenKhc: {
+            100: '#17794D',
+            200: '#075431',
+        },
+    },
+});
 
 export default function App() {
     return (
         <BrowserRouter>
-            <ChakraProvider>
+            <ChakraProvider theme={theme}>
                 <Header />
                 <Routes></Routes>
+                <Footer />
             </ChakraProvider>
         </BrowserRouter>
     );
