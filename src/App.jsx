0bce75c1252d89fb91fed0775682d09df15c74fc
Yeren Kalibek <yerenn22@gmail.com>
2023-01-03 23:50:24 +0600

    feat: add language provider and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import Footer from './layouts/Footer';
 import Header from './layouts/Header';
 
-import Login from './routes/Login';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
 
-const theme = extendTheme({
-    colors: {
-        brand: {
-            yellow: {
-                50: '#fbf8ef',
-                100: '#f1e9ce',
-                200: '#ebdfb6',
-                300: '#e2d095',
-                400: '#dcc781',
-                500: '#d3b961',
-                600: '#c0a858',
-                700: '#968345',
-                800: '#746635',
-                900: '#594e29',
-            },
-            green: {
-                50: '#e8f2ed',
-                100: '#b7d5c8',
-                200: '#94c1ad',
-                300: '#64a588',
-                400: '#459471',
-                500: '#17794d',
-                600: '#156e46',
-                700: '#105637',
-                800: '#0d432a',
-                900: '#0a3320',
-            },
-        },
-    },
-});
+import { LanguageProvider } from './context/languageContext';
+import { theme } from './lib/theme';
 
 export default function App() {
     return (
         <BrowserRouter>
             <ChakraProvider theme={theme}>
-                <Header />
-                <Routes>
-                    <Route path='/' element={<Login />} />
-                </Routes>
-                <Footer />
+                <LanguageProvider>
+                    <Header />
+                    <Routes>
+                        <Route path='/' element={<SignIn />} />
+                        <Route path='/sign-up' element={<SignUp />} />
+                    </Routes>
+                    <Footer />
+                </LanguageProvider>
             </ChakraProvider>
         </BrowserRouter>
     );
