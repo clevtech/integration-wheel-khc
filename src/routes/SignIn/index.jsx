73a2d76d55d7dd0c91debafb3b566dc1c11721e6
Yeren Kalibek <yerenn22@gmail.com>
2023-01-03 23:50:47 +0600

    feat: add sign in and sign up routes

---
+++ src/routes/SignIn/index.jsx
@@ -0,0 +1,13 @@
+import { Box, Container, Stack } from '@chakra-ui/react';
+
+export default function SignIn() {
+    return (
+        <Box>
+            <Container maxWidth='container.xl' py={{ base: '12' }}>
+                <Stack>
+                    
+                </Stack>
+            </Container>
+        </Box>
+    );
+}
---
+++ src/routes/SignUp/index.jsx
@@ -0,0 +1,13 @@
+import { Box, Container, Stack } from '@chakra-ui/react';
+
+export default function SignUp() {
+    return (
+        <Box>
+            <Container maxWidth='container.xl' py={{ base: '12' }}>
+                <Stack>
+                    
+                </Stack>
+            </Container>
+        </Box>
+    );
+}
