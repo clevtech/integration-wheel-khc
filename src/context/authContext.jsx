3a3568ff501db3468ffb44bac41bc947ed47ab59
Yeren Kalibek <yerenn22@gmail.com>
2023-01-05 06:10:28 +0600

    feat: start work on auth context

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,10 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { useJwt } from 'react-jwt';
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
-    const [user, setUser] = useState({});
-    const [tokens, setTokens] = useState({});
+    const [cookies, setCookie, removeCookie] = useCookies(['tokens', 'user']);
+
+    const [tokens, setTokens] = useState(cookies.tokens || {});
+    const [user, setUser] = useState(cookies.user || {});
+
+    const handleSignIn = ({ email, password }) => {
+        // const { decodedToken, isExpired } = useJwt(token);
+    };
+
+    const handleSignUp = () => {
+        setTokens({});
+        setUser({});
+
+        removeCookie('tokens');
+        removeCookie('user');
+    };
+
+    const handleRefresh = () => {};
+
+    const value = { user, tokens, handleSignIn, handleSignUp, handleRefresh };
 
     return (
         <AuthContext.Provider value={{ user, setUser, tokens, setTokens }}>
