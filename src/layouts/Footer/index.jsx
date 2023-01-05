b83c12acfd2bcdb02859a09b650ad704264d4b3a
Yeren Kalibek <yerenn22@gmail.com>
2023-01-05 21:00:39 +0600

    feat: add translations

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -1,18 +1,22 @@
+import { useTranslation } from 'react-i18next';
+import { IoLogoVk, IoLogoInstagram, IoLogoFacebook } from 'react-icons/io5';
 import {
     Box,
     ButtonGroup,
     Container,
     Divider,
     IconButton,
+    VStack,
     Stack,
     Text,
 } from '@chakra-ui/react';
-import { IoLogoVk, IoLogoInstagram, IoLogoFacebook } from 'react-icons/io5';
 
 import { Baiterek } from '../../assets/icons/Baiterek';
 import { KazakhstanHousingCompany } from '../../assets/icons/KazakhstanHousingCompany';
 
 export default function Footer() {
+    const { t } = useTranslation();
+
     const companyLinks = [
         {
             label: 'baiterek-icon-button',
@@ -45,18 +49,21 @@ export default function Footer() {
     ];
 
     return (
-        <Box as='footer' maxWidth='100%' background='brand.green.500'>
+        <Box as='footer' background='brand.green.500'>
             <Container maxWidth='container.xl' color='white'>
                 <Stack
+                    align={{
+                        base: 'flex-start',
+                        sm: 'center',
+                    }}
                     direction={{
                         base: 'column',
                         sm: 'row',
                     }}
                     justify='space-between'
-                    spacing='24px'
-                    py='24px'
+                    py='6'
                 >
-                    <ButtonGroup spacing='24px' variant='outline'>
+                    <ButtonGroup spacing='3' variant='outline'>
                         {companyLinks.map((link, index) => (
                             <IconButton
                                 key={index}
@@ -70,12 +77,14 @@ export default function Footer() {
                             />
                         ))}
                     </ButtonGroup>
-                    <Text>
-                        г. Астана <br /> пр. Мәңгілік Ел, 55А
-                    </Text>
-                    <Text>
-                        +7 (7172) 79 75 75 <br /> info@khc.kz
-                    </Text>
+                    <VStack align='flex-start'>
+                        <Text>{t('layouts.footer.city')}</Text>
+                        <Text>{t('layouts.footer.address')}</Text>
+                    </VStack>
+                    <VStack align='flex-start'>
+                        <Text>+7 (7172) 79 75 75</Text>
+                        <Text>info@khc.kz</Text>
+                    </VStack>
                 </Stack>
                 <Divider />
                 <Stack
@@ -85,13 +94,9 @@ export default function Footer() {
                         lg: 'row',
                     }}
                     justify='space-between'
-                    py='24px'
+                    py='6'
                 >
-                    <Text align='center'>
-                        &copy; 2004-{new Date().getFullYear()} AO «Казахстанская
-                        Жилищная Компания», дочерняя организация АО «НУХ
-                        «Байтерек»​
-                    </Text>
+                    <Text align='center'>{t('layouts.footer.copyright')}</Text>
                     <ButtonGroup spacing='8px' variant='outline'>
                         {socialLinks.map((link, index) => (
                             <IconButton
