84e35570f09bb621cd13e03c7525e92fa191b072
Yeren Kalibek <yerenn22@gmail.com>
2023-01-05 22:50:24 +0600

    feat: add auth context

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -2,16 +2,58 @@ import { createContext, useContext, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { useJwt } from 'react-jwt';
 
+import { login } from '../services/authApi';
+
 export const AuthContext = createContext();
 
-export default function AuthProvider({ children }) {
+const AuthProvider = ({ children }) => {
     const [cookies, setCookie, removeCookie] = useCookies(['tokens', 'user']);
 
     const [tokens, setTokens] = useState(cookies.tokens || {});
     const [user, setUser] = useState(cookies.user || {});
 
-    const handleSignIn = ({ email, password }) => {
-        // const { decodedToken, isExpired } = useJwt(token);
+    const handleSignIn = ({ email, password, rememberMe }) => {
+        login({ email, password }).then((response) => {
+            const { data } = response;
+
+            const { accessToken } = data;
+
+            const { decodedToken } = useJwt(accessToken);
+
+            const { name } = decodedToken;
+
+            const expirationInSeconds = rememberMe ? 31536000000 : 3600000;
+
+            setTokens({ accessToken, refreshToken });
+            setUser({ name });
+
+            setCookie(
+                'tokens',
+                { accessToken },
+                {
+                    path: '/',
+                    expires: new Date(Date.now() + expirationInSeconds),
+                    maxAge: expirationInSeconds,
+                    domain: 'localhost',
+                    secure: false,
+                    httpOnly: false,
+                    sameSite: 'lax',
+                }
+            );
+            setCookie(
+                'user',
+                { name },
+                {
+                    path: '/',
+                    expires: new Date(Date.now() + expirationInSeconds),
+                    maxAge: expirationInSeconds,
+                    domain: 'localhost',
+                    secure: false,
+                    httpOnly: false,
+                    sameSite: 'lax',
+                }
+            );
+        });
     };
 
     const handleSignUp = () => {
@@ -27,8 +69,18 @@ export default function AuthProvider({ children }) {
     const value = { user, tokens, handleSignIn, handleSignUp, handleRefresh };
 
     return (
-        <AuthContext.Provider value={{ user, setUser, tokens, setTokens }}>
-            {children}
-        </AuthContext.Provider>
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
     );
-}
+};
+
+const useAuth = () => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error('useAuth must be used within a AuthProvider');
+    }
+
+    return context;
+};
+
+export { AuthProvider, useAuth };
