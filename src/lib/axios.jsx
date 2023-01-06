b47ed0eb2610530dd7d6ed0c22f2472692ed3533
Yeren Kalibek <yerenn22@gmail.com>
2023-01-06 23:06:31 +0600

    feat: change base axios obj

--- src/lib/axios.jsx
+++ src/lib/axios.jsx
@@ -1,33 +1,21 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
+const api = axios.create({
     baseURL: 'http://193.106.99.147:32777/api/v1',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    timeout: 5000,
-    timeoutErrorMessage: 'Request timed out',
-    withCredentials: true,
 });
 
-const getRequest = async (url) => {
-    return apiClient.get(url).then((response) => response);
+const errorHandler = (error) => {
+    const statusCode = error.response?.status;
+
+    if (statusCode) {
+        console.error(error);
+    }
+
+    return Promise.reject(error);
 };
 
-const postRequest = async (url, payload) => {
-    return apiClient.post(url, payload).then((response) => response);
-};
+api.interceptors.response.use(undefined, (error) => {
+    return errorHandler(error);
+});
 
-const putRequest = async (url, payload) => {
-    return apiClient.put(url, payload).then((response) => response);
-};
-
-const patchRequest = async (url, payload) => {
-    return apiClient.patch(url, payload).then((response) => response);
-};
-
-const deleteRequest = async (url) => {
-    return apiClient.delete(url).then((response) => response);
-};
-
-export { getRequest, postRequest, putRequest, patchRequest, deleteRequest };
+export { api };
