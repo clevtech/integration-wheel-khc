bc0ac3c3ff15403c07ad78f0a9411322263c9f65
Yeren Kalibek <yerenn22@gmail.com>
2023-01-26 18:46:39 +0600

    feat: add base url using env

--- src/lib/axios.jsx
+++ src/lib/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://193.106.99.147:32777/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 const errorHandler = (error) => {
