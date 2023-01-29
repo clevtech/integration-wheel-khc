9fc6fe51375bf79871f6be344b28313485df981d
Yeren Kalibek <yerenn22@gmail.com>
2023-01-29 18:28:53 +0600

    feat: add pagination and action buttons

--- src/routes/History.jsx
+++ src/routes/History.jsx
@@ -1,60 +1,154 @@
-import { useEffect, useState } from 'react';
-import { Badge, Container, Table, TableContainer, Tbody, Thead, Td, Th, Tr } from '@chakra-ui/react';
+import { useEffect, useMemo, useState } from 'react';
+
+import {
+  Badge,
+  Button,
+  Container,
+  HStack,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverFooter,
+  PopoverHeader,
+  PopoverTrigger,
+  Table,
+  TableContainer,
+  Tbody,
+  Thead,
+  Td,
+  Th,
+  Tooltip,
+  Tr,
+  useToast,
+} from '@chakra-ui/react';
+import { IoEyeOutline, IoReloadOutline, IoTrashOutline } from 'react-icons/io5';
+
+import Pagination from '../components/Layout/Pagination';
 
 import { useAuth } from '../context/authContext';
 import { tasks as tasksApi } from '../services/tasks';
 
 export default function History() {
-    const [tasks, setTasks] = useState([]);
+  const toast = useToast();
+  const { tokens, user } = useAuth();
 
-    const { tokens } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        tasksApi.getAll(tokens.accessToken).then((res) => {
-            const { data } = res;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
 
-            setTasks(data);
+  useEffect(() => {
+    setIsLoading(true);
+
+    tasksApi
+      .getAll(tokens.accessToken)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch(() => {
+        toast({
+          title: 'Ошибка',
+          description: 'Не удалось загрузить данные',
+          status: 'error',
+          colorScheme: 'brand.red',
+          duration: 5000,
+          isClosable: true,
         });
-    }, []);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    return (
-        <Container maxWidth='container.xl'>
-            <TableContainer>
-                <Table colorScheme='brand.green'>
-                    <Thead>
-                        <Tr>
-                            <Th>Название услуги</Th>
-                            <Th>E-mail пользователя</Th>
-                            <Th>Дата запроса</Th>
-                            <Th>Статус запроса</Th>
-                            <Th>Время обработки запроса</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {tasks.map((task, index) => (
-                            <Tr key={index}>
-                                <Td>{task.providerRequestName}</Td>
-                                <Td>{task.userEmail}</Td>
-                                <Td>{task.requestDate}</Td>
-                                <Td>
-                                    <Badge
-                                        colorScheme={
-                                            task.requestStatus === 'COMPLETED'
-                                                ? 'brand.green'
-                                                : task.requestStatus === 'PENDING'
-                                                ? 'brand.yellow'
-                                                : 'brand.red'
-                                        }
-                                    >
-                                        {task.requestStatus}
-                                    </Badge>
-                                </Td>
-                                <Td>{task.responseTime}</Td>
-                            </Tr>
-                        ))}
-                    </Tbody>
-                </Table>
-            </TableContainer>
-        </Container>
-    );
+  const currentPageData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * pageSize;
+    const lastPageIndex = firstPageIndex + pageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, data]);
+
+  return (
+    <Container maxWidth='container.xl'>
+      {!isLoading ? (
+        <>
+          <TableContainer>
+            <Table colorScheme='brand.green'>
+              <Thead>
+                <Tr>
+                  <Th>Название услуги</Th>
+                  <Th>E-mail</Th>
+                  <Th>Дата</Th>
+                  <Th>Статус</Th>
+                  <Th>Время обработки</Th>
+                  {user.role === 'ADMIN' && <Th>Действия</Th>}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {currentPageData.map((task, index) => (
+                  <Tr key={index}>
+                    <Td>{task.providerRequestName}</Td>
+                    <Td>{task.userEmail}</Td>
+                    <Td>{task.requestDate}</Td>
+                    <Td>
+                      <Badge
+                        colorScheme={
+                          task.requestStatus === 'COMPLETED'
+                            ? 'brand.green'
+                            : task.requestStatus === 'PENDING'
+                            ? 'brand.yellow'
+                            : 'brand.red'
+                        }
+                      >
+                        {task.requestStatus}
+                      </Badge>
+                    </Td>
+                    <Td>{task.responseTime / 1000} с</Td>
+                    {user.role === 'ADMIN' && (
+                      <Td>
+                        <HStack>
+                          <Popover>
+                            <PopoverTrigger>
+                              <Button colorScheme='brand.green'>
+                                <Tooltip label='Повторить запрос' aria-label='Повторить запрос'>
+                                  <IoEyeOutline />
+                                </Tooltip>
+                              </Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                              <PopoverArrow />
+                              <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+                            </PopoverContent>
+                          </Popover>
+                          <Tooltip label='Повторить запрос' aria-label='Повторить запрос'>
+                            <Button colorScheme='brand.yellow'>
+                              <IoReloadOutline />
+                            </Button>
+                          </Tooltip>
+                          <Tooltip label='Удалить запрос' aria-label='Повторить запрос'>
+                            <Button colorScheme='brand.red'>
+                              <IoTrashOutline />
+                            </Button>
+                          </Tooltip>
+                        </HStack>
+                      </Td>
+                    )}
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+          <Pagination
+            currentPage={currentPage}
+            totalCount={data.length}
+            pageSize={pageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+        </>
+      ) : (
+        <h1>Загрузка...</h1>
+      )}
+    </Container>
+  );
 }
